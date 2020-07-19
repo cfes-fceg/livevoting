9688346e196ee498fe1917aad78e8279844f4e44
Alex Stojda <alex.stojda@appdirect.com>
2020-07-19 15:33:13 -0400

    #7: Add basic question factory & seeder

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Question::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'is_active' => $faker->boolean
+    ];
+});
