951c502198512a09908dfdec701adc5f6fdaa84d
Alex Stojda <alex.stojda@appdirect.com>
2020-07-19 15:33:13 -0400

    #7: Add basic question factory & seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              UsersSeeder::class,
-             EngSocsSeeder::class
+             EngSocsSeeder::class,
+             QuestionSeeder::class
          ]);
     }
 }
