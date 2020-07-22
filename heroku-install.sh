90e8a3f8bc1a29c49ffc0b70721e1979b10d6594
Alex Stojda <alex.stojda@appdirect.com>
2020-07-22 09:34:47 -0400

    #15: move npm run prod to heroku-postbuild npm script

--- heroku-install.sh
+++ heroku-install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 if [ $APP_ENV == 'local' ] || [ $APP_ENV == 'staging'  ] ; then
-    npm run prod
     composer install --dev
     composer mfs
 else
     php artisan migrate
 fi
+
+php artisan passport:install
